0740289385a2e7af895843461273b8bac64f031f
adorableyou26 <adorableyou26@gmail.com>
2023-05-12 17:05:29 +0900

    커밋/풀리퀘스트 후 CI/CD 테스트
    jar 실행 테스트

--- deploy.sh
+++ deploy.sh
@@ -21,4 +21,4 @@ echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls $REPOSITORY/target/ | grep '.jar' | grep -v '.original')
 JAR_PATH=$REPOSITORY/target/$JAR_NAME
 
-nohup java -jar $JAR_PATH &
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
