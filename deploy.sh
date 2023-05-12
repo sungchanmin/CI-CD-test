4b440eaca14e90e05670d9764c76300d594c54d8
26 <adorableyou26@gmail.com>
2023-05-12 12:07:04 +0900

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+!/usr/bin/env bash
+
+REPOSITORY=/home/ubuntu/applications/ExampleAwsCodeDeploy
+APP_NAME=ExampleAwsCodeDeploy
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+CURRENT_PID=$(pgrep -f $APP_NAME)
+echo "$CURRENT_PID"
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 실행중인 해당 애플리케이션이 없습니다. "
+else
+  echo "> kill -9 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> 새 어플리케이션 배포"
+
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
