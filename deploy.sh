795348410971794e2e3d5add121ef983b0d0bad4
adorableyou26 <adorableyou26@gmail.com>
2023-05-16 09:26:24 +0900

    Merge remote-tracking branch 'origin/master'

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
-APP_NAME=begin_vegan
 
-
-echo "> Check the currently running container"
-CONTAINER_ID=$(docker ps -aqf "name=$APP_NAME")
-
-if [ -z "$CONTAINER_ID" ];
-then
-  echo "> No such container is running."
-else
-  echo "> Stop and remove container: $CONTAINER_ID"
-  docker stop "$CONTAINER_ID"
-  docker rm "$CONTAINER_ID"
-fi
-
-echo "> Remove previous Docker image"
-docker rmi "$APP_NAME"
-
-echo "> Build Docker image"
-docker build -t "$APP_NAME" .
-
-echo "> Run the Docker container"
-docker run -d -p 3000:8080 --name "$APP_NAME" "$APP_NAME"
+#APP_NAME=begin_vegan
+#
+#
+#echo "> Check the currently running container"
+#CONTAINER_ID=$(docker ps -aqf "name=$APP_NAME")
+#
+#if [ -z "$CONTAINER_ID" ];
+#then
+#  echo "> No such container is running."
+#else
+#  echo "> Stop and remove container: $CONTAINER_ID"
+#  docker stop "$CONTAINER_ID"
+#  docker rm "$CONTAINER_ID"
+#fi
+#
+#echo "> Remove previous Docker image"
+#docker rmi "$APP_NAME"
+#
+#echo "> Build Docker image"
+#docker build -t "$APP_NAME" .
+#
+#echo "> Run the Docker container"
+#docker run -d -p 3000:8080 --name "$APP_NAME" "$APP_NAME"
 
