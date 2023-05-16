0ff808abf0b849396910d20e14a322d01b91f253
adorableyou26 <adorableyou26@gmail.com>
2023-05-16 09:24:03 +0900

    Merge remote-tracking branch 'origin/master'

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,25 @@
 #!/usr/bin/env bash
 APP_NAME=begin_vegan
 
-touch logCheck
 
-echo "> Check the currently running container" >> logCheck
+echo "> Check the currently running container"
 CONTAINER_ID=$(docker ps -aqf "name=$APP_NAME")
-echo "$CONTAINER_ID" >> logCheck
 
 if [ -z "$CONTAINER_ID" ];
 then
-  echo "> No such container is running." >> logCheck
+  echo "> No such container is running."
 else
-  echo "> Stop and remove container: $CONTAINER_ID" >> logCheck
+  echo "> Stop and remove container: $CONTAINER_ID"
   docker stop "$CONTAINER_ID"
   docker rm "$CONTAINER_ID"
 fi
 
-echo "> Remove previous Docker image" >> logCheck
+echo "> Remove previous Docker image"
 docker rmi "$APP_NAME"
 
-echo "> Build Docker image" >> logCheck
+echo "> Build Docker image"
 docker build -t "$APP_NAME" .
 
-echo "> Run the Docker container" >> logCheck
+echo "> Run the Docker container"
 docker run -d -p 3000:8080 --name "$APP_NAME" "$APP_NAME"
 
